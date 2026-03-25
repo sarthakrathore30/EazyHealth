d60ca6306ec552a84fd7ba8aeb2538501bc5678a
Sarthak Singh Rathore <sarthak.work30@gmail.com>
2026-03-25 15:49:42 +0530

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,19 +7,50 @@ from .emergency import EmergencyDetector, EmergencyAssessment, EmergencyLevel
 # Import feedback manager
 try:
     from .feedback import FeedbackManager
+    _has_feedback = True
+except ImportError:
+    _has_feedback = False
+
+# Import vitals manager
+try:
+    from .vitals import VitalsManager
+    _has_vitals = True
+except ImportError:
+    _has_vitals = False
+
+if _has_feedback and _has_vitals:
     __all__ = [
-        'InputSanitizer', 
-        'RequestValidator', 
-        'EmergencyDetector', 
-        'EmergencyAssessment', 
+        'InputSanitizer',
+        'RequestValidator',
+        'EmergencyDetector',
+        'EmergencyAssessment',
+        'EmergencyLevel',
+        'FeedbackManager',
+        'VitalsManager'
+    ]
+elif _has_feedback:
+    __all__ = [
+        'InputSanitizer',
+        'RequestValidator',
+        'EmergencyDetector',
+        'EmergencyAssessment',
         'EmergencyLevel',
         'FeedbackManager'
     ]
-except ImportError:
+elif _has_vitals:
     __all__ = [
-        'InputSanitizer', 
-        'RequestValidator', 
-        'EmergencyDetector', 
-        'EmergencyAssessment', 
+        'InputSanitizer',
+        'RequestValidator',
+        'EmergencyDetector',
+        'EmergencyAssessment',
+        'EmergencyLevel',
+        'VitalsManager'
+    ]
+else:
+    __all__ = [
+        'InputSanitizer',
+        'RequestValidator',
+        'EmergencyDetector',
+        'EmergencyAssessment',
         'EmergencyLevel'
     ]
